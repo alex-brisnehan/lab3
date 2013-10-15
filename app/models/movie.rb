90374324d0c19210d05299cb1c5cb5709e2d90e9
saasbook <saasbook@saasbook.(none)>
2013-10-15 00:24:49 -0700

    Updated movie_controller

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,8 @@
+# Edited by: Alex Brisnehan
 class Movie < ActiveRecord::Base
+
+	# Added the ratings selections
+
     def self.all_ratings
         self.find(:all, :select => "rating", :group => "rating").map(&:rating)
     end
